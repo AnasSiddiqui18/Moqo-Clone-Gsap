905a6ecb1895d289f1a36ac4a3c4a9c41e3a581b
Anas Siddiqui <anassiddiqui0910@gmail.com>
2023-09-07 02:14:08 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { gsap } from "gsap";
+import { useEffect } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  useEffect(() => {
+    const animation = gsap.to(".page1 h1", {
+      transform: "translateX(-100%)",
+      fontWeight: 100,
+
+      scrollTrigger: {
+        trigger: ".page1",
+        scroller: "body",
+        markers: true,
+        start: "top 0",
+        end: "top -200%",
+        scrub: 1,
+        pin: true,
+      },
+    });
+
+    return () => {
+      animation.kill();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="page1">
+        <h1>We build brands</h1>
+      </div>
+      <div className="page2"></div>
+      <div className="page3"></div>
+    </>
+  );
+};
+
+export default App;

61ab296f0d952d4581a1979de9460ef46c58c097
Anas Siddiqui <anassiddiqui0910@gmail.com>
2023-09-07 02:31:05 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
     const animation = gsap.to(".page1 h1", {
       transform: "translateX(-100%)",
       fontWeight: 100,
-
       scrollTrigger: {
         trigger: ".page1",
         scroller: "body",

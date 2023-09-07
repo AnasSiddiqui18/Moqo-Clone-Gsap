7bd9f498b930f6206da3eaf3b452217894f02026
Anas Siddiqui <anassiddiqui0910@gmail.com>
2023-09-07 13:32:29 +0500

    Added a banana

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,67 @@ gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   useEffect(() => {
-    const animation = gsap.to(".page1 h1", {
+    const textAnimation = gsap.to(".page1 h1", {
       transform: "translateX(-100%)",
       fontWeight: 100,
       scrollTrigger: {
         trigger: ".page1",
         scroller: "body",
         scrub: 3,
+        top: "top 0",
+        end: "top -200%",
         pin: true,
       },
     });
 
+    const boxAnimation = gsap.fromTo(
+      ".box img",
+      {
+        opacity: 0,
+        y: "0",
+        // rotate: "0",
+      },
+      {
+        y: "-800",
+        // rotate: "-360deg",
+        opacity: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".box", // Add a box-trigger element
+          scroller: "body",
+          scrub: 3,
+          markers: true,
+          start: "top 30%", // Adjust this value to control when the box appears
+          end: "top 50%", // Adjust this value to control when the box stops appearing
+        },
+      }
+    );
+
     return () => {
-      animation.kill();
+      textAnimation.kill();
+      boxAnimation.kill();
     };
   }, []);
 
   return (
     <>
+      <header>
+        <nav>
+          <div className="logo">
+            <h1>MOQO™</h1>
+          </div>
+          <ul>
+            <li>Work</li>
+            <li>Contact</li>
+            <li>Menu</li>
+          </ul>
+        </nav>
+      </header>
       <div className="page1">
         <h1>We build brands</h1>
+        <div className="box">
+          <img src="./public/img/kela.png" alt="" />
+        </div>
       </div>
       <div className="page2"></div>
       <div className="page3"></div>
